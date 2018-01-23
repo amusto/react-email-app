359cd31ffcd9545831b4cb240145c9f3ccad0ac7
Armando Musto <armando.musto@gmail.com>
2018-01-23 16:09:39 -0500

    Change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express(); // Generates a running express app
 
 app.get('/', (req, res) => {
-    res.send({hi: "There!!!"});
+    res.send({hi: "There Buddy!!!"});
 });
 
 const PORT = process.env.PORT || 5000;
