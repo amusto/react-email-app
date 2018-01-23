82856f73998ec654861ce7fee7f79df64428a740
Armando Musto <armando.musto@gmail.com>
2018-01-23 15:09:42 -0500

    fix

---
+++ index.js
@@ -0,0 +1,10 @@
+// Common modules are better supported by Node over ES2016 modules
+const express = require('express');
+const app = express(); // Generates a running express app
+
+app.get('/', (req, res) => {
+    res.send({hi: "there"});
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Running app on port: ${PORT}`));
