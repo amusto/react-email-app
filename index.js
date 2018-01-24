f510ba61710d1880d60019c90a8e5600bf98a387
Armando Musto <armando.musto@gmail.com>
2018-01-24 16:13:41 -0500

    Working google auth with Passport

--- index.js
+++ index.js
@@ -2,10 +2,32 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express(); // Generates a running express app
 
-passport.use(new GoogleStrategy()); //INFO: Let passport know that we want to use the google strategy
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+        console.log('access token', accessToken);
+        console.log('refresh token', refreshToken);
+        console.log('profile:', profile);
+    }
+  )
+);
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+/*app.get('/auth/google/callback', (req, res) => {
+    res.send('Success');
+});*/
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Running app on port: ${PORT}`));
