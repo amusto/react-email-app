c638f4ec403571eb66ee94d89d7bd9c89058e3c7
Armando Musto <armando.musto@gmail.com>
2018-01-24 14:17:21 -0500

    recent

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID: '1042394417495-0himiguuf7s86qtdju6k1shdahne8oqc.apps.googleusercontent.com',
+    googleClientSecret: 'Gcyf6YIg2Ik_dGL4LdcTdV-P'
+};
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Common modules are better supported by Node over ES2016 modules
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express(); // Generates a running express app
 
-app.get('/', (req, res) => {
-    res.send({hi: "There Buddy!!!"});
-});
+passport.use(new GoogleStrategy()); //INFO: Let passport know that we want to use the google strategy
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Running app on port: ${PORT}`));
