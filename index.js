711b66e230c2d9e33ca51fbccca7e2f729cf6ddf
Armando Musto <armando.musto@gmail.com>
2018-01-27 12:40:57 -0500

    Test enable trust proxy

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.enable("trust proxy");
+
 require('./routes/commonRoutes')(app);
 require('./routes/authRoutes')(app);
 
