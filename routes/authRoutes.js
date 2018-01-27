cd6e88d59cb911acb53cb8e7b846120a7181855d
Armando Musto <armando.musto@gmail.com>
2018-01-27 11:45:24 -0500

    Completed auth functions

---
+++ routes/authRoutes.js
@@ -0,0 +1,29 @@
+/*
+    File: authRoutes.js
+    Description: Client authentication is provided via Passport.js and maintained with cookies
+*/
+
+
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+
+module.exports = (app) => {
+    app.get('/auth/google', passport.authenticate('google', {
+            scope: ['profile', 'email']
+        })
+    );
+
+    app.get('/auth/google/callback', passport.authenticate('google'));
+
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send(req.user);
+    });
+
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user);
+    });
+
+};
