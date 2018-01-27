4af9cfd4cca132f4dfe8f1bfc0fb8840140197d1
Armando Musto <armando.musto@gmail.com>
2018-01-27 12:14:54 -0500

    Updating keys files

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '1042394417495-0himiguuf7s86qtdju6k1shdahne8oqc.apps.googleusercontent.com',
+    googleClientSecret: 'Gcyf6YIg2Ik_dGL4LdcTdV-P',
+    mongoURI: 'mongodb://appUser:Nanakarobi321@ds115758.mlab.com:15758/emaily-dev',
+    cookieKey: 'MonkeyBread09437575674567764905675496'
+};
