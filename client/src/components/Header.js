6658dedfd6ffc062d1803e1b25432c6cd90776a4
Armando Musto <armando.musto@gmail.com>
2018-02-06 18:31:53 -0500

    Added billing and client side react app

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Payments from './Payments'
 
+// Helper function that displays login OR logout in header
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                  <li><a href="/auth/google">Login with Google</a></li>
+                );
+            default:
+                return [
+                    <li key="1"><Payments /></li>,
+                    <li key="2" style={{ margin: '0 10px' }}> Credits: {this.props.auth.credits}</li>,
+                    <li key="3"><a href="/api/logout">Logout</a></li>
+                ]
+        }
+    }
+
     render() {
         return (
           <nav>
               <div className="nav-wrapper">
-                  <a href="/" className="left brand-logo">Emaily</a>
+                  <Link
+                    to={this.props.auth ? '/surveys' : '/'}
+                    className="left brand-logo"
+                    style={{ margin: '0 10px' }}>
+                      Emaily</Link>
                   <ul className="right">
-                      <li>
-                          <a href="/auth/google">Login with Google</a>
-                      </li>
-
+                      {this.renderContent()}
                   </ul>
               </div>
           </nav>
         )
     }
 }
-export default Header;
+
+function mapStateToProps({ auth }) {
+    return { auth }
+}
+
+export default connect(mapStateToProps)(Header);
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -10,6 +11,8 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express(); // Generates a running express app
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
@@ -23,8 +26,24 @@ app.use(passport.session());
 
 app.enable("trust proxy");
 
-require('./routes/commonRoutes')(app);
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+    console.log('Running in Production');
+    // Express will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express will serve up the index.html file as the default if it can't find it in our route files above
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+
+
+} else {
+    console.log('Running in Development');
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Running app on port: ${PORT}`));
